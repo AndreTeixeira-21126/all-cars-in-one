79e6771fd1c850d30221496f628776428a9f82fa
AndreTeixeira-21126 <a21126@alunos.ipca.pt>
2023-10-26 10:13:53 +0100

    chore: created User.create method which creates an user assuring that is a valid user

--- apps/auth/src/entities/User.js
+++ apps/auth/src/entities/User.js
@@ -6,9 +6,30 @@ class User {
      * @param {*} password password of user
      * @param {*} id id of user, can be a integer, an uuid or a string
      */
-  constructor (name, email, password, id) {
+  constructor (name, email, password, id = undefined) {
     this.id = id
     this.name = name
+    this.email = email
+    this.password = password
+  }
+
+  /**
+   * @description Create a new instance of User making the necessary validations
+   * @param {*} name Name of user
+   * @param {*} email Email of User
+   * @param {*} password Password of User
+   * @param {*} id Id of User
+   * @returns a new instance of User
+   */
+
+  static create (name, email, password, id = undefined) {
+    if (name.length === 0) {
+      throw new Error('Name is required')
+    }
+    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email) === false) {
+      throw new Error('Email is required')
+    }
+    return new User(name, email, password, id)
   }
 }
 
