cc9cbdbf1cf1b7b64f122e714a59c44cb1736412
AndreTeixeira-21126 <a21126@alunos.ipca.pt>
2023-10-28 11:09:15 +0100

    chore: created dockerfile and docker-compose file

--- apps/auth/server.js
+++ apps/auth/server.js
@@ -2,9 +2,10 @@ const makeApp = require('./src/appBuilder')
 const dotenv = require('dotenv')
 const InMemoryUserRepository = require('./src/repositories/InMemoryUserRepository')
 
-dotenv.config()
+//dotenv.config()
+
 const app = makeApp(new InMemoryUserRepository())
 
-app.listen(process.env.SERVER_PORT || 3000, () => {
+app.listen(process.env.SERVER_PORT, () => {
   console.log(`Server is running on http://localhost:${process.env.SERVER_PORT || 3000}/`)
 })
